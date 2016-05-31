ac1a948141a31fcf021e31ea3ad39755a57e03b9
sunandiyer <sunandiyer@Sunands-MBP.home>
2016-05-31 16:23:42 -0500

    Updating main File

--- VC_Regression.py
+++ VC_Regression.py
@@ -16,95 +16,8 @@ from sklearn.cross_validation import train_test_split
 from sklearn import linear_model
 
 #load data from files
-dealData = pd.read_csv("COOLEY-VC_TOTAL_DEAL_VOLUME.csv")
-indexData = pd.read_csv("NASDAQOMX-NDXT.csv")
-exitData = pd.read_csv("NVCA-VC_EXITS_QUARTERLY.csv")
-
-#get the dates
-dealDates = dealData.iloc[:,0]
-indexDates = indexData.iloc[:,0]
-exitDates = exitData.iloc[:,0]
-
-#get dates for both deal and exit data
-datesBoth = np.zeros(len(dealDates)).astype(str)
-for i in range(len(dealDates)):
-    for j in range(len(exitDates)):
-        if (dealDates.iloc[i] == exitDates.iloc[j]):
-            datesBoth[i] = dealDates.iloc[i]
-datesBoth = datesBoth[datesBoth != '0.0']
-
-#get index dates
-dates3 = np.zeros(len(datesBoth)).astype(str)
-for i in range(len(datesBoth)):
-    for j in range(len(indexDates)):
-        if(datesBoth[i] == indexDates.iloc[j]):
-            dates3[i] = datesBoth[i]
-dates3 = dates3[dates3 != '0.0']
-
-#get the index values for the deal data
-dealIndexes = np.zeros(len(dealDates))
-for i in range(len(dealDates)):
-    for j in range(len(dates3)):
-        if(dealDates.iloc[i] == dates3[j]):
-            dealIndexes[i] = i
-dealIndexes = dealIndexes[dealIndexes > 0]
-
-#get the index values for exit data
-exitIndexes = np.zeros(len(exitDates))
-for i in range(len(exitDates)):
-    for j in range(len(dates3)):
-        if(exitDates.iloc[i] == dates3[j]):
-            exitIndexes[i] = i
-
-#have to do this since index 0 is needed for exit data
-exitIndexes[0] = 1
-exitIndexes = exitIndexes[exitIndexes > 0]
-exitIndexes[0] = 0.
-
-#get the average MA values
-averageMAList = np.zeros(len(dates3))
-for i in range(len(exitIndexes)):
-    index = int(exitIndexes[i])
-    averageMAList[i] = exitData.iloc[index, 4]
-    
-#get the average IPO values
-averageIPOList = np.zeros(len(dates3))
-for i in range(len(exitIndexes)):
-    index = int(exitIndexes[i])
-    averageIPOList[i] = exitData.iloc[index, 7]
-    
-#get the values of the index    
-indexVals = np.zeros(len(dates3))
-for i in range(len(exitIndexes)):
-    index = int(exitIndexes[i])
-    indexVals[i] = indexData.iloc[index, 1]
-
-#get the amount raised    
-raisedVals = np.zeros(len(dates3))
-for i in range(len(exitIndexes)):
-    index = int(exitIndexes[i])
-    raisedVals[i] = dealData.iloc[index, 2]
-
-#reshape the data    
-raisedVals.shape = 25,1
-indexVals.shape = 25,1
-averageIPOList.shape = 25,1
-averageMAList.shape = 25,1
-singleColumn = np.ones(25)
-singleColumn.shape = 25,1
-
-#convert the final data to a matrix
-finalMat = np.matrix(np.hstack([singleColumn, raisedVals,averageIPOList, 
-                                averageMAList, indexVals]))
-
-#convert to a Data Frame
-finalDF = pd.DataFrame(finalMat)
-finalDF.columns = ["Single_Column", "Amount_Raised", "Average_IPO_Value", 
-"Average_MA_Value", "Index_Value"]
-
-testArray = np.array(finalMat)
-
-
+data = pd.read_csv("output.csv")
+testArray = np.array(data)
 
 #get data ready for gradient descent
 X = testArray[:,:4]
@@ -137,7 +50,18 @@ X_normTest = rf.normNewVals(X_test, mu, sigma)
 y_test.shape = len(y_test),1
 totError = (abs(np.dot(X_normTest, theta) - y_test)/y_test).sum()/len(y_test)
 
+
+plt.xlabel("Prediction")
+plt.ylabel("Actual Value")
+plt.title("Prediction vs. Actual")
+plt.scatter(np.dot(X_normTest,theta), y_test)
+plt.show()
+plt.clf()
+
 #Linear Regression using Sci-Kit Learn
 regr = linear_model.LinearRegression()
 regr.fit(X_normTrain, y_train)
-
+plt.ylabel("My Prediction")
+plt.xlabel("Sci-Kit Learn Prediction")
+plt.scatter(regr.predict(X_normTest), np.dot(X_normTest, theta))
+plt.show()
